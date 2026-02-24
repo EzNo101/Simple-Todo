cef56292989af1be54026cef57b629a56b2f9f9e
EzNo101 <sshamb2v@gmail.com>
2026-02-24 21:54:24 +0100

    alembic added to project

--- app/main.py
+++ app/main.py
@@ -1,17 +1,7 @@
-from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from app.api import router
-from app.database.base import Base
-from app.database.session import engine
-from app.models.todo import Todo
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-        yield
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 app.include_router(router)
