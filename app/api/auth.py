bc0fcf92d3f4a691ea91a0f7959940b5288acd7c
EzNo101 <sshamb2v@gmail.com>
2026-02-26 21:29:31 +0100

    authorization works in swagger now

--- app/api/auth.py
+++ app/api/auth.py
@@ -5,6 +5,7 @@ from app.database.session import get_db
 from app.repository.user import UserRepository
 from app.services.user import UserService
 from app.schemas.user import UserCreate, UserUpdate, UserResponse, UserLogin, Token
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -14,7 +15,7 @@ async def register(user: UserCreate, session: AsyncSession = Depends(get_db)):
     return await service.register(user)
 
 @router.post("/login", response_model=Token, status_code=200)
-async def login(data: UserLogin, session: AsyncSession = Depends(get_db)):
+async def login(form_data: OAuth2PasswordRequestForm = Depends(), session: AsyncSession = Depends(get_db)):
     service = UserService(UserRepository(session))
-    token = await service.login(data.email, data.password)
+    token = await service.login(form_data.username, form_data.password)
     return {"access_token": token, "token_type": "bearer"}
